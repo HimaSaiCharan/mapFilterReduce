38082aab10300b2890eb9cea2f687ced37ac2482
HimaSaiCharan <127401464+HimaSaiCharan@users.noreply.github.com>
2024-12-22 17:46:24 +0530

    6 functions done

--- filter.js
+++ filter.js
@@ -16,48 +16,46 @@ const filterLongWords = function (words) {
   return words.filter(sizeGreaterThan5);
 };
 
-const isPropertyGreaterThan = function (threshold, property) {
-  return function (object) {
-    return object[property] > threshold;
-  };
+const olderThan30 = function (person) {
+  return person.age > 30;
 };
 
 // people older than 30 [{name: "Alice", age: 25}, {name: "Bob", age: 35}] => [{name: "Bob", age: 35}]
 const filterAdults = function (people) {
-  return people.filter(isPropertyGreaterThan(30, 'age'));
+  return people.filter(olderThan30);
 };
 
-const isPropertyTrue = function (property) {
-  return function (object) {
-    return object[property];
-  };
+const isActive = function (user) {
+  return user.active;
 };
 
 // active users [{username: "alice", active: true}, {username: "bob", active: false}] => [{username: "alice", active: true}]
 const filterActiveUsers = function (users) {
-  return users.filter(isPropertyTrue('active'));
+  return users.filter(isActive);
 };
 
-const isGreaterThan = function (number) {
+const greaterThan10 = function (number) {
   return number > 10;
 };
 
 // numbers greater than 10 [5, 12, 7, 18, 3] => [12, 18]
 const filterNumbersGreaterThanTen = function (numbers) {
-  return numbers.filter(isGreaterThan);
+  return numbers.filter(greaterThan10);
+};
+
+const bookWithMorePages = function (book) {
+  return book.pages > 200;
 };
 
 // books with more than 200 pages [{title: "Book 1", pages: 150}, {title: "Book 2", pages: 250}] => [{title: "Book 2", pages: 250}]
 const filterLongBooks = function (books) {
-  return books.filter(isPropertyGreaterThan(200, 'pages'));
+  return books.filter(bookWithMorePages);
 };
+
+console.log(filterLongBooks([{ title: "Book 1", pages: 150 }, { title: "Book 2", pages: 250 }]));
 
 // users with incomplete profiles [{username: "alice", profileComplete: true}, {username: "bob", profileComplete: false}] => [{username: "bob", profileComplete: false}]
-const filterIncompleteProfiles = function (users) {
-  return users.filter(isPropertyTrue('profileComplete'));
-};
-
-// console.log(filterIncompleteProfiles([{ username: "alice", profileComplete: true }, { username: "bob", profileComplete: false }]));
+const filterIncompleteProfiles = function (users) { };
 
 // students with grades above 80 [{name: "John", grade: 75}, {name: "Jane", grade: 85}] => [{name: "Jane", grade: 85}]
 const filterHighGrades = function (students) { };
